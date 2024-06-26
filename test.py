0d61ec27264d635f994cc0f994efeb9989f9f4ef
unknown <surajraj2109@gmail.com>
2024-06-27 00:19:38 +0530

    upd

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-@patch('TSC.TableauAuth')
-    @patch('TSC.Server')
-    def test_get_signin_info(self, mock_server, mock_tableau_auth):
-        instance = TableauDataExtractor()
-        instance.userName = 'username'
-        instance.get_password = MagicMock(return_value='password')
-        instance.siteUrl = 'siteUrl'
-        instance.server = 'server'
-        instance.get_signin_info()
-        mock_tableau_auth.assert_called_once_with('username', 'password', 'siteUrl')
-        mock_server.assert_called_once_with('server')
+@patch.object(tableau_api_lib.TSC, 'TableauAuth')
+@patch.object(tableau_api_lib.TSC, 'Server')
+def test_get_signin_info(self, MockServer, MockAuth):
+    mock_auth = MagicMock()
+    MockAuth.return_value = mock_auth
+    mock_server = MagicMock()
+    MockServer.return_value = mock_server
+    
+    self.instance.get_signin_info()
+    
+    MockAuth.assert_called_once_with(self.userName, self.instance.get_password(), self.siteUrl)
+    MockServer.assert_called_once()
+    
+    # Additional verification: Check the arguments passed to Server
+    actual_server_args, _ = mock_server.call_args
+    self.assertEqual(actual_server_args[0], self.server)
