e45036f9e0bb20a948e195b73a79e8fc67120936
unknown <surajraj2109@gmail.com>
2024-06-26 23:57:18 +0530

    upd

--- test.py
+++ test.py
@@ -1,17 +1,11 @@
-@patch('io.BytesIO')
-    @patch('zipfile.ZipFile')
-    def test_convert_and_create_zip(self, mock_zipfile, mock_bytesio):
+@patch('TSC.TableauAuth')
+    @patch('TSC.Server')
+    def test_get_signin_info(self, mock_server, mock_tableau_auth):
         instance = TableauDataExtractor()
-        viewDf = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-        viewName = 'test_view'
-        mock_io_instance = MagicMock()
-        mock_bytesio.return_value = mock_io_instance
-        mock_zipfile_instance = MagicMock()
-        mock_zipfile.return_value.__enter__.return_value = mock_zipfile_instance
-        mock_csv_string = "col1,col2\n1,3\n2,4\n"
-        viewDf.to_csv = MagicMock(return_value=mock_csv_string)
-        data = instance.convert_and_create_zip(viewDf, viewName)
-        viewDf.to_csv.assert_called_once()
-        mock_zipfile_instance.writestr.assert_called_once_with(f"{viewName.strip()}.csv", mock_csv_string)
-        mock_io_instance.getvalue.assert_called_once()
-        mock_io_instance.close.assert_called_once()
+        instance.userName = 'username'
+        instance.get_password = MagicMock(return_value='password')
+        instance.siteUrl = 'siteUrl'
+        instance.server = 'server'
+        instance.get_signin_info()
+        mock_tableau_auth.assert_called_once_with('username', 'password', 'siteUrl')
+        mock_server.assert_called_once_with('server')
