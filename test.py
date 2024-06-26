0e8e49a6381e00a07393f604e6bf4c26161a9a86
unknown <surajraj2109@gmail.com>
2024-06-26 23:31:55 +0530

    upd

--- test.py
+++ test.py
@@ -1,22 +1,17 @@
-@patch.object(MyClass, 'get_request')
-    def test_get_all_views(self, mock_get_request):
-        instance = MyClass()
-        mock_response_1 = MagicMock()
-        mock_response_1.json.return_value = {
-            'views': {'view': [{'id': 1}, {'id': 2}]},
-            'pagination': {'totalAvailable': 4}
-        }
-        mock_response_2 = MagicMock()
-        mock_response_2.json.return_value = {
-            'views': {'view': [{'id': 3}, {'id': 4}]},
-            'pagination': {'totalAvailable': 4}
-        }
-        mock_get_request.side_effect = [mock_response_1, mock_response_2]
-
-        total_views = instance.get_all_views()
-
-        self.assertEqual(len(total_views), 4)
-        self.assertEqual(total_views, [{'id': 1}, {'id': 2}, {'id': 3}, {'id': 4}])
-        self.assertEqual(mock_get_request.call_count, 2)
-        mock_get_request.assert_any_call(1)
-        mock_get_request.assert_any_call(2)
+@patch('io.BytesIO')
+    @patch('zipfile.ZipFile')
+    def test_convert_and_create_zip(self, mock_zipfile, mock_bytesio):
+        instance = TableauDataExtractor()
+        viewDf = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+        viewName = 'test_view'
+        mock_io_instance = MagicMock()
+        mock_bytesio.return_value = mock_io_instance
+        mock_zipfile_instance = MagicMock()
+        mock_zipfile.return_value.__enter__.return_value = mock_zipfile_instance
+        mock_csv_string = "col1,col2\n1,3\n2,4\n"
+        viewDf.to_csv = MagicMock(return_value=mock_csv_string)
+        data = instance.convert_and_create_zip(viewDf, viewName)
+        viewDf.to_csv.assert_called_once()
+        mock_zipfile_instance.writestr.assert_called_once_with(f"{viewName.strip()}.csv", mock_csv_string)
+        mock_io_instance.getvalue.assert_called_once()
+        mock_io_instance.close.assert_called_once()
