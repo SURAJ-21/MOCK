888176530e1adf5af5a91b2d99f29a88c295dab9
unknown <surajraj2109@gmail.com>
2024-06-28 22:39:03 +0530

    upd

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-bid_61ba9568-a5a0-11ee-9a58-b496910f6f06
-cid_368d725c-a5a0-11ee-805a-b496910f6f06
+def setUp(self):
+        self.serviceName = MagicMock()
+        self.reportType = 'reportType'
+        self.handler = BaseHandler()
+    
+    @patch('BaseHandler.getEnvConfig')
+    def test_initialize(self, MockGetEnvConfig):
+        mock_env = MagicMock()
+        self.serviceName._env = mock_env
+        mock_wcap_config = MagicMock()
+        MockGetEnvConfig.return_value = mock_wcap_config
+        
+        self.handler.initialize(self.serviceName, self.reportType)
+        
+        self.assertEqual(self.handler.regressionService, self.serviceName)
+        self.assertEqual(self.handler.reportType, self.reportType)
+        self.assertEqual(self.handler.entitlementsService, self.serviceName.getEntitlementsService())
+        self.assertEqual(self.handler._env, mock_env)
+        self.assertEqual(self.handler._wcapConfig, mock_wcap_config)
+        self.assertEqual(MockGetEnvConfig.call_count, 1)
