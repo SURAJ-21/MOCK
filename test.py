932e2eb1b9fb407780cbebadc647d84702434a8a
unknown <surajraj2109@gmail.com>
2024-07-01 08:57:10 +0530

    upd

--- test.py
+++ test.py
@@ -1,34 +1,40 @@
 def setUp(self):
-        self.instance = TableauRegressionShell()
-        self.instance._paramsDict = {'dashboardParams': ['param1', 'param2', 'param3']}
-        self.instance.tableauRegressionConfigurer = MagicMock()
-        self.instance.setResponse = MagicMock()
-
-    def test_downloadBatchAnalysisView_success(self):
-        baselineDetails = {'viewName': 'TestView', '_sa_instance_state': 'to_remove'}
-        expected_baselineDetails = {
-            'viewName': 'TestView',
-            'dashboardParams': ['param1', 'param2', 'param3'],
-            'dashboardType': 'BatchAnalysis'
+        self.handler = RegressionServiceHandler()
+        self.handler._paramsDict = {
+            'some_key': 'some_value'
         }
         
-        self.instance.tableauRegressionConfigurer.is_view_in_inventory.return_value = baselineDetails
-        self.instance.tableauRegressionConfigurer.download_view.return_value = 'zippedFile'
+        # Mock objects
+        self.obj_write = MagicMock()
+        self.obj_finish = MagicMock()
+        self.obj_set_status = MagicMock()
+        
+        # Patch methods with mock objects
+        patch('your_module.RegressionServiceHandler.write', self.obj_write).start()
+        patch('your_module.RegressionServiceHandler.finish', self.obj_finish).start()
+        patch('your_module.RegressionServiceHandler.set_status', self.obj_set_status).start()
+        
+        # Add cleanup to stop patching
+        self.addCleanup(patch.stopall)
 
-        self.instance.downloadBatchAnalysisView()
+    @patch('your_module.RegressionServiceHandler.removeRegressionConfig')
+    @patch('your_module.queryRegressionConfigTable')
+    def test_deleteRegression_baseline_configured(self, mock_queryTable, mock_removeConfig):
+        # Mock is_baseline_configured to return a non-None value
+        mock_config_row = MagicMock()
+        mock_config_row.baselineTaskId = 'test_task_id'
+        mock_config_row.env = 'test_env'
+        mock_queryTable.is_baseline_configured.return_value = mock_config_row
 
-        self.assertEqual(self.instance.tableauRegressionConfigurer.is_view_in_inventory.call_count, 1)
-        self.assertEqual(self.instance.tableauRegressionConfigurer.download_view.call_count, 1)
-        self.instance.tableauRegressionConfigurer.download_view.assert_called_with(expected_baselineDetails)
-        self.assertEqual(self.instance.setResponse.call_count, 1)
-        self.instance.setResponse.assert_called_with('zippedFile')
+        # Mock removeRegressionConfig to return a response
+        mock_removeConfig.return_value = 'test_response'
 
-    def test_downloadBatchAnalysisView_invalid_params(self):
-        self.instance._paramsDict = {'dashboardParams': ['param1', 'param2']}
+        self.handler.deleteRegression()
 
-        with self.assertRaises(Exception) as context:
-            self.instance.downloadBatchAnalysisView()
-        self.assertEqual(str(context.exception), "Item in the dashBoardParams has to be of length 3. Received: ['param1', 'param2']")
-
-if __name__ == '__main__':
-    unittest.main()
+        # Assertions
+        mock_queryTable.is_baseline_configured.assert_called_once_with(self.handler._paramsDict)
+        mock_removeConfig.assert_called_once_with('test_task_id')
+        mock_queryTable.deleteBaseline.assert_called_once_with('test_task_id', 'test_env')
+        self.obj_set_status.assert_called_once_with(200)
+        self.obj_write.assert_called_once_with('test_response')
+        self.obj_finish.assert_called_once()
