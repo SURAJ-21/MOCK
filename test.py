4a7b635c045e5d8d484b5e870d912842598718cb
unknown <surajraj2109@gmail.com>
2024-07-01 22:09:12 +0530

    upd

--- test.py
+++ test.py
@@ -1,40 +1,25 @@
 def setUp(self):
         self.handler = RegressionServiceHandler()
-        self.handler._paramsDict = {
-            'some_key': 'some_value'
-        }
-        
-        # Mock objects
-        self.obj_write = MagicMock()
-        self.obj_finish = MagicMock()
-        self.obj_set_status = MagicMock()
-        
-        # Patch methods with mock objects
-        patch('your_module.RegressionServiceHandler.write', self.obj_write).start()
-        patch('your_module.RegressionServiceHandler.finish', self.obj_finish).start()
-        patch('your_module.RegressionServiceHandler.set_status', self.obj_set_status).start()
-        
-        # Add cleanup to stop patching
-        self.addCleanup(patch.stopall)
+        self.handler.readParams = MagicMock()
+        self.handler.request = MagicMock()
+        self.handler.request.path = "/some/path/getInventory"
+        self.handler.getInventory = MagicMock(return_value="inventory_result")
+        self.handler.getRegressionConfig = MagicMock(return_value="regression_config_result")
 
-    @patch('your_module.RegressionServiceHandler.removeRegressionConfig')
-    @patch('your_module.queryRegressionConfigTable')
-    def test_deleteRegression_baseline_configured(self, mock_queryTable, mock_removeConfig):
-        # Mock is_baseline_configured to return a non-None value
-        mock_config_row = MagicMock()
-        mock_config_row.baselineTaskId = 'test_task_id'
-        mock_config_row.env = 'test_env'
-        mock_queryTable.is_baseline_configured.return_value = mock_config_row
+    @patch('your_module.RegressionServiceHandler.getInventory')
+    @patch('your_module.RegressionServiceHandler.getRegressionConfig')
+    def test_get_inventory(self, mock_get_regression_config, mock_get_inventory):
+        self.handler.request.path = "/some/path/getInventory"
+        result = self.handler.get()
+        mock_get_inventory.assert_called_once()
+        mock_get_regression_config.assert_not_called()
+        self.assertEqual(result, "inventory_result")
 
-        # Mock removeRegressionConfig to return a response
-        mock_removeConfig.return_value = 'test_response'
-
-        self.handler.deleteRegression()
-
-        # Assertions
-        mock_queryTable.is_baseline_configured.assert_called_once_with(self.handler._paramsDict)
-        mock_removeConfig.assert_called_once_with('test_task_id')
-        mock_queryTable.deleteBaseline.assert_called_once_with('test_task_id', 'test_env')
-        self.obj_set_status.assert_called_once_with(200)
-        self.obj_write.assert_called_once_with('test_response')
-        self.obj_finish.assert_called_once()
+    @patch('your_module.RegressionServiceHandler.getInventory')
+    @patch('your_module.RegressionServiceHandler.getRegressionConfig')
+    def test_get_regression_config(self, mock_get_regression_config, mock_get_inventory):
+        self.handler.request.path = "/some/path/getRegressionConfig"
+        result = self.handler.get()
+        mock_get_inventory.assert_not_called()
+        mock_get_regression_config.assert_called_once()
+        self.assertEqual(result, "regression_config_result")
