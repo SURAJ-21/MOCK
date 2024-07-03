ae4e0d74a388c976ca41294f535ee53a46bf28f5
unknown <surajraj2109@gmail.com>
2024-07-04 00:44:27 +0530

    upd

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-Limited Functionality: The current Tableau regression functionality is restricted to a few dashboards and lacks test coverage, preventing scalability to user dashboards.
-User Dependency: Users cannot configure or run regressions independently, relying on the tech team for changes and updates.
-Automation Need: There's no record of Tableau dashboards configured for regression testing, necessitating automation and data destruction for obsolete dashboards.
-
-Tableau Usage: WCAP team uses Tableau for visualizing and analyzing loan loss forecast results.
-Modifications: Developers must ensure that changes in parameters, such as adding columns or changing formulas, do not affect other results.
-Regression Testing: Implement regression testing to ensure reliability, allowing users to view, configure, and run regression tests for all dashboards.
+Inventory Management: Designed an inventory system for all Tableau dashboards, updating nightly to track new, deleted, or updated dashboards, including a separate inventory for regression-ready dashboards.
+Automated Regression: Implemented a nightly job to run regressions on all configured dashboards, using Swagger to expose endpoints for user interaction, enabling them to view, baseline, and delete existing configurations.
+Enhanced User Access: Provided users with downloadable CSV formats of Tableau dashboard views, stored as Parquet files on S3, allowing them to download, store, configure baselines, and run regressions for all types of dashboards.
