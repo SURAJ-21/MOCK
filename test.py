80bc7026783a93ab4fd8e5af9cc382d78889d81d
unknown <surajraj2109@gmail.com>
2024-07-04 23:56:05 +0530

    upd

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-Learned effective interaction with colleagues and the importance of asking for help without hesitation.
-Gained experience with large databases and understood the significance of test coverage and unit testing.
-Observed real examples of managing large databases using pagination.
-Recognized the responsibility that comes with having permissions and the need to be extra cautious when altering data.
-Emphasized the importance of double-checking table names before making changes.
-Learned the value of not reinventing the wheel and focusing on component reusability.
+Add this to the novaReg UI.
+Create a job that runs regression tests for the dashboards configured for regression.
+Discuss with Soumya B to determine if we can onboard the current users to Hydra.
+Investigate issues with the functional accounts onboarded to Hydra.
+Test the changes in UAT.
+Talk to Matt about the DRD.
+Configure all baselines for Data Restoration and Destruction (DRD).
