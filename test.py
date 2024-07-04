ff0c637afa1c639530c9dd27d41638c5eaca31d6
unknown <surajraj2109@gmail.com>
2024-07-05 00:51:46 +0530

    upd

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-Add this to the novaReg UI.
-Create a job that runs regression tests for the dashboards configured for regression.
-Discuss with Soumya B to determine if we can onboard the current users to Hydra.
-Investigate issues with the functional accounts onboarded to Hydra.
-Test the changes in UAT.
-Talk to Matt about the DRD.
-Configure all baselines for Data Restoration and Destruction (DRD).
+Tableau Regression Service
+
+GET /getInventory: To get all the workbooks and views present on Tableau.
+GET /getRegressionConfig: To get all the views and workbooks which are configured for Regression.
+POST /downloadTableauBatchAnalysisViewAsCSV: To download the specified view of BatchAnalysis Dashboard present on Tableau.
+POST /downloadTableauVarianceViewAsCSV: To download the specified view of Variance Dashboard present on Tableau.
+POST /configureRegressionBaselineBatchAnalysisView: To configure baseline for BatchAnalysis Dashboards present on Tableau.
+POST /configureRegressionBaselineVarianceView: To configure baseline for Variance Dashboards present on Tableau.
+DELETE /deleteRegressionConfiguration: To download the specified view of the workbook present in Tableau Dashboard.
+POST /runRegression: To run Regression using regression-utility-service.
+
+
+
+
+
